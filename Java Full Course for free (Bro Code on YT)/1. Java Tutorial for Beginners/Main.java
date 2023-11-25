cdce29e9fad6544fd3f10cba294a17d578d42960
Jonathan Agarrado (Desktop) <jet320boy@gmail.com>
2023-11-25 20:38:52 +0800

    Initial commit

---
+++ Java Full Course for free (Bro Code on YT)/1. Java Tutorial for Beginners/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("\nHello, World!\n");
+
+        String personName = "Jonathan";
+        System.out.println("I am " + personName + ".\nNice to meet you!\n");
+    }
+}
